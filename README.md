fa766319d135cf1cd56dc9d349a4877addf8b5c7
stephenhky <stephenhky@yahoo.com.hk>
2016-08-25 13:26:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# ahlearn: Life-long Learning
+To import any methods or classes in the library, type
+
+```
+from ahlearn import money
+from ahlearn import SoM
+from ahlearn import CoM
+```
---
+++ ahlearn/base.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def money():
+    return np.random.power(1.69, 1)
+
+def SoM():
+    return np.zeros((1,))
+
+def CoM():
+    return np.sum(np.random.power(1.69, 100))
